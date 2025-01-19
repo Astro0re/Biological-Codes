bc0f1f1aed49f225703f7abf9106958665453ad3
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-01-19 18:03:16 +0100

    Update

--- Bio Code.r
+++ Bio Code.r
@@ -3,6 +3,8 @@ D_Gen<- function(){
   purine <- c("A","T")
   pyramidine <- c("G","C")
   gene <- c(purine,pyramidine)
-  sample(gene, size = 10, replace = TRUE)
+  sample(gene, size = 100, replace = TRUE) 
 }
 D_Gen()
+#Count
+table(D_Gen())
