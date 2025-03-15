c1dffd30a187571eede0377a9f2cb01c6709f622
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-03-15 13:32:35 +0100

    Code Update

--- Relational Skeletal System Data Base.py
+++ Relational Skeletal System Data Base.py
@@ -1,61 +1,63 @@
-# Relational Skeletal System Function(Connect Searched for bone to every other bone in articulation)
+# Relational Skeletal System Data Base(Connect Searched for bone to every other bone in articulation)
 # Duplicate for Organs?
 
 
 def identify_bone():
     bone = input("Enter Bone: ")
-    Shoulder ={'Clavicle','Scapula','Acromion'}
-    Arm ={'Humerus','Radius','Ulna'}
-    Phalanges ={'Proximal','Middle','Distal'}
-    Carpals = {'Scaphoid', 'Lunate', 'Triquetrum', 'Pisiform', 'Trapezium', 'Trapezoid', 'Capitate', 'Hamate'}
-    Hand= {'Metacarpals', Phalanges, Carpals}
-    leg = {'Femur', 'Patella', 'Tibia', 'Fibula'}
-    Ankle = {'Talus', 'Calcaneus', 'Navicular', 'Cuboid', 'Cuneiforms'}
-    Foot = {[Ankle, 'Metatarsals', Phalanges]}
-    Neo_Cranium = {'Frontal', 'Parietal', 'Temporal', 'Occipital', 'Sphenoid', 'Ethmoid'}
-    Viscero_Cranium = {'Maxilla', 'Mandible', 'Zygomatic', 'Nasal', 'Lacrimal', 'Palatine', 'Inferior Nasal Concha',
-                       'Vomer', 'Turbinate bones'}
-    Auditory_Ossicles = {'Malleus', 'Incus', 'Stapes'}
-    Skull = {Neo_Cranium, Viscero_Cranium, Auditory_Ossicles}
-    Neck = {'Cranial Vertebrae', 'Cervical Vertebrae'}
-    Sternum = {'Manubrium', 'Body', 'Xiphoid Process'}
-    Ribs = {'True Ribs', 'False Ribs', 'Floating Ribs'}
-    Thoracic = {Sternum, Ribs, 'Thoracic Vertebrae', 'Lumbar Vertebrae'}
-    Pelvis = {'Ilium', 'Ischium', 'Pubis', 'Sacrum', 'Coccyx'}
+    Shoulder = set['Clavicle', 'Scapula', 'Acromion']
+    Arm = set['Humerus', 'Radius', 'Ulna']
+    Phalanges = set['Proximal', 'Middle', 'Distal']
+    Carpals = set['Scaphoid', 'Lunate', 'Triquetrum', 'Pisiform', 'Trapezium', 'Trapezoid', 'Capitate', 'Hamate']
+    Hand = set['Metacarpals', Phalanges, Carpals]
+    leg = set['Femur', 'Patella', 'Tibia', 'Fibula']
+    Ankle = set['Talus', 'Calcaneus', 'Navicular', 'Cuboid', 'Cuneiforms']
+    Foot = set[Ankle, 'Metatarsals', Phalanges]
+    Neo_Cranium = set['Frontal', 'Parietal', 'Temporal', 'Occipital', 'Sphenoid', 'Ethmoid']
+    Viscero_Cranium = set[
+        'Maxilla', 'Mandible', 'Zygomatic', 'Nasal', 'Lacrimal', 'Palatine', 'Inferior Nasal Concha', 'Vomer', 'Turbinate bones']
+    Auditory_Ossicles = set['Malleus', 'Incus', 'Stapes']
+    Skull = set[Neo_Cranium, Viscero_Cranium, Auditory_Ossicles]
+    Neck = set['Cranial Vertebrae', 'Cervical Vertebrae']
+    Sternum = set['Manubrium', 'Body', 'Xiphoid Process']
+    Ribs = set['True Ribs', 'False Ribs', 'Floating Ribs']
+    Thoracic = set[Sternum, Ribs, 'Thoracic Vertebrae', 'Lumbar Vertebrae']
+    Pelvis = set['Ilium', 'Ischium', 'Pubis', 'Sacrum', 'Coccyx']
 
-    Upper_Limb = {Shoulder, Arm, Hand}
+    Upper_Limb = set[Shoulder, Arm, Hand]
 
-    Lower_Limb = {leg, Foot}
+    Lower_Limb = set[leg, Foot]
 
-    Head_Neck = {Skull, Neck}
+    Head_Neck = set[Skull, Neck]
 
-    Thoracic_Pelvis = {Thoracic, Pelvis}
+    Thoracic_Pelvis = set[Thoracic, Pelvis]
 
-    Skeletal_System = {Upper_Limb, Lower_Limb, Head_Neck, Thoracic_Pelvis}
+    Skeletal_System = set[Upper_Limb, Lower_Limb, Head_Neck, Thoracic_Pelvis]
 
     Found = []
     Not_Found = []
-    #articulation_count =int(input("How Many Bone's Articulate with this: "))
-    #input("List the Articulations: ")
+    # articulation_count =int(input("How Many Bone's Articulate with this: "))
+    # input("List the Articulations: ")
     for i in Skeletal_System:
         for j in i:
             for k in j:
-                if bone == i or j or  k:
+                if bone == i or j or k:
                     Found.append(bone)
-                    print(Found)
+                    print(f"Found {Found}")
                 else:
                     Not_Found.append(bone)
-                    print(Not_Found)
+                    print(f"Did not find {Not_Found}")
+
 
 ## Might have to turn the list into a dictionary to make it easier to search for the bone
-        
-    #for i in Skeletal_System:
-     #   if len(Skeletal_System) == articulation_count:
-      #      return i, "Correct"
-       # else:
-        #    return "Incorrect"
-        
+
+# for i in Skeletal_System:
+#   if len(Skeletal_System) == articulation_count:
+#      return i, "Correct"
+# else:
+#    return "Incorrect"
+
 
 identify_bone()
 
 # review code
+# Not Found output not working
