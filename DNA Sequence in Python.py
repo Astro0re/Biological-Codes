a40b896f6f9ccf9918225d4d719fffbc3195df97
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-04-02 12:56:06 +0100

    Code Update

--- DNA Sequence in Python.py
+++ DNA Sequence in Python.py
@@ -9,5 +9,23 @@ def DNA(var):
         else:
             return var
 
+def Count_DNA(var):
+    A_Cou=[]
+    G_Cou=[]
+    T_Cou=[]
+    C_Cou=[]
+    for i in var:
+        if i == 'A':
+            A_Cou.append(i)
+        elif i == 'G':
+            G_Cou.append(i)
+        elif i == 'T':
+            T_Cou.append(i)
+        elif i == 'C':
+            C_Cou.append(i)
+    print(f' A = {len(A)} , G = {len(G_Cou)}, T = {len(T_Cou)}, C = {len(C_Cou)}')
+
+
 b= ['A', 'T']
 DNA(b)
+Count_DNA(b)
