0515ff8974de22cb52a1070c84e827fc1f4029a0
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-04-05 19:15:10 +0100

    Code Update

--- Medical Form For Patients.py
+++ Medical Form For Patients.py
@@ -1,5 +1,8 @@
 # Medical Form For Patients
 # For more convenient and easy to use management of medical records
+import random
+
+
 def BIO_Form():
     Sickel_Cell = []
     O = []
@@ -7,6 +10,7 @@ def BIO_Form():
     B = []
     AB = []
     Urgent = []
+    patient_id = random.randint(1-9999, 0)
     print("Please input the necessary information...")
     name = input("Name: ")
     sex = input("Sex: ")
@@ -15,7 +19,7 @@ def BIO_Form():
     blood_group = input("Blood Group: ")
     ill = input("Known Illnesses: ")
     comp = input("Complaint?: ")
-    blood_pressure = input(int("Blood Pressure: "))
+    blood_pressure = input(int("Blood Pressure(Bps): "))
 
     #first_observed = input("First Observation: ")
     #appointment =input("Appointment: ")
@@ -25,9 +29,10 @@ def BIO_Form():
      #   print("Please Make An Appointment.")
       #  print("Your Information will be stored and used when you return during your appointment.")
     print(f"Welcome {name}, Thank you for your information, you will attend to you shortly.")
+    print(f"Please note your patient identification number is {patient_id}")
     if genotype == "SS" or "SC":
         Sickel_Cell.append(name)
-    if blood_pressure > 170:
+    if int(blood_pressure) > 170 :
         Urgent.append(name)
         print(f'This Patient requires urgent treatment')
     if comp == "Emergency":
