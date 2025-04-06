3b97baac01f96f9c235f61391dac011dbd4da867
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-04-06 22:43:13 +0100

    Code Update

--- Relational Skeletal System Data Base.py
+++ Relational Skeletal System Data Base.py
@@ -4,23 +4,39 @@
 
 def identify_bone():
     bone = input("Enter Bone: ")
+
     Shoulder = set['Clavicle', 'Scapula', 'Acromion']
+
     Arm = set['Humerus', 'Radius', 'Ulna']
+
     Phalanges = set['Proximal', 'Middle', 'Distal']
+
     Carpals = set['Scaphoid', 'Lunate', 'Triquetrum', 'Pisiform', 'Trapezium', 'Trapezoid', 'Capitate', 'Hamate']
+
     Hand = set['Metacarpals', Phalanges, Carpals]
+
     leg = set['Femur', 'Patella', 'Tibia', 'Fibula']
+
     Ankle = set['Talus', 'Calcaneus', 'Navicular', 'Cuboid', 'Cuneiforms']
+
     Foot = set[Ankle, 'Metatarsals', Phalanges]
+
     Neo_Cranium = set['Frontal', 'Parietal', 'Temporal', 'Occipital', 'Sphenoid', 'Ethmoid']
-    Viscero_Cranium = set[
-        'Maxilla', 'Mandible', 'Zygomatic', 'Nasal', 'Lacrimal', 'Palatine', 'Inferior Nasal Concha', 'Vomer', 'Turbinate bones']
+
+    Viscero_Cranium = set['Maxilla', 'Mandible', 'Zygomatic', 'Nasal', 'Lacrimal', 'Palatine', 'Inferior Nasal Concha', 'Vomer', 'Turbinate bones']
+
     Auditory_Ossicles = set['Malleus', 'Incus', 'Stapes']
+
     Skull = set[Neo_Cranium, Viscero_Cranium, Auditory_Ossicles]
+
     Neck = set['Cranial Vertebrae', 'Cervical Vertebrae']
+
     Sternum = set['Manubrium', 'Body', 'Xiphoid Process']
+
     Ribs = set['True Ribs', 'False Ribs', 'Floating Ribs']
+
     Thoracic = set[Sternum, Ribs, 'Thoracic Vertebrae', 'Lumbar Vertebrae']
+
     Pelvis = set['Ilium', 'Ischium', 'Pubis', 'Sacrum', 'Coccyx']
 
     Upper_Limb = set[Shoulder, Arm, Hand]
@@ -34,8 +50,7 @@ def identify_bone():
     Skeletal_System = set[Upper_Limb, Lower_Limb, Head_Neck, Thoracic_Pelvis]
 
     Found = []
-    # articulation_count =int(input("How Many Bone's Articulate with this: "))
-    # input("List the Articulations: ")
+
     for i in Skeletal_System:
         for j in i:
             for k in j:
@@ -46,13 +61,9 @@ def identify_bone():
                     print(f"Wrong Input")
 
 
-## Might have to turn the list into a dictionary to make it easier to search for the bone
+## Might have to turn the set into a dictionary to make it easier to search for the bone
 
-# for i in Skeletal_System:
-#   if len(Skeletal_System) == articulation_count:
-#      return i, "Correct"
-# else:
-#    return "Incorrect"
+
 
 
 identify_bone()
