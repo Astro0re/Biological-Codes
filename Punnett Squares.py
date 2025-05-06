25c94f3ef446579e5d9e7506f73613f347f67f51
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-05-06 19:30:07 +0100

    Code Cleanup and interactions

--- Punnett Squares.py
+++ Punnett Squares.py
@@ -2,7 +2,18 @@
 # To predict distribution of parent genotype across a single generation
 from numpy.ma.core import append
 
-def punn(var1,var2):
+def punn_simple(var1,var2):
+    print("Matching Genes...")
+    print("Sorting possible combinations...")
+    chan1 = var1[0],var2[0]
+    chan2 = var1[0],var2[1]
+    chan3 = var1[1],var2[0]
+    chan4 = var1[1],var2[1]
+    print("Possible Combinations", chan1, chan2, chan3, chan4)
+
+def punn_multi(var1,var2):
+    print("Matching Genes...")
+    print("Sorting possible combinations...")
     chan1 = var1[0],var2[0]
     chan2 = var1[0],var2[1]
     chan3 = var1[1],var2[0]
@@ -26,7 +37,6 @@ def punn(var1,var2):
     gen14_2 = chan2[0], chan4[1]
     gen15_2 = chan2[1], chan4[0]
     gen16_2 = chan2[1], chan4[1]
-    print("Possible Combinations", chan1, chan2, chan3, chan4)
     print(f"Proceeding Generations {gen1_2},{gen2_2},{gen3_2},{gen4_2},"
           f"{gen5_2},{gen6_2},{gen7_2},{gen8_2},{gen9_2},{gen10_2},{gen11_2},"
           f"{gen12_2},{gen13_2},{gen14_2},{gen15_2},{gen16_2}")
@@ -34,6 +44,7 @@ def punn(var1,var2):
 # Genotype of male and female 
 M=["A","a"]
 F= ["A","a"]
-print( punn(M,F))
+print( punn_simple(M,F))
+print( punn_multi(M,F))
 
 
