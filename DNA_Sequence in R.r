e9b8959251b9a7803d86dee1327dfae7d58fcaec
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-06-13 14:29:30 +0100

    Validate sequence in R

--- DNA_Sequence in R.r
+++ DNA_Sequence in R.r
@@ -1,4 +1,4 @@
-#DNA combinations generator
+#DNA Sequence generator
 D_Gen<- function(){
   print("Generating Sequence...")
   purine <- c("A","T")
@@ -7,4 +7,15 @@ D_Gen<- function(){
   seq <- sample(gene, size = 100, replace = TRUE)
   print("Counting Sequence...")
   print(table(seq))
+}
+
+#DNA Sequence Validator
+D_Val<- function(Seq){
+  print("Validatig Sequence...")
+  purine <- c("A","T")
+  pyramidine <- c("G","C")
+  gene <- c(purine,pyramidine)
+  for i in Seq:
+    if i != gene[i]
+    print ("invalid Sequence")
 }
