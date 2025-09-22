39bf8c56fa047b8746f914dc244b96216d6b8c9e
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-22 10:30:06 +0100

    Dataset classification and model plan

--- Genotype Identification.py
+++ Genotype Identification.py
@@ -14,10 +14,28 @@ class __main__:
 # Source Hugging Face [Docty](https://huggingface.co/Docty)
 global df
 df = pd.read_parquet("hf://datasets/Docty/Blood-Cells/data/train-00000-of-00001.parquet") 
+print(df.head())
+# Dataset conatins 8 labels for diffrent categories of blood cells
+# 0 : Monocytes
+# 1 : ig
+# 2 : Neutrophils
+# 3 : Basophils
+# 4 : Lymphocytes
+# 5 : Erythroblasts
+# 6 : Eosinophils
+# 7 : Platlets
+
 
 # Filter out only Erythrocytes (Red Blood cells)
 global Eryth 
-Eryth =[]
+Eryth = df[df['label'] == 5]
+
+# Transform the image data into a numpy array
+
+# Train Model on the Erythrocytes
+
+# Using a simple CNN model for image classification
+
 
 # bit classification of Erythrocytes
 global bit_count
