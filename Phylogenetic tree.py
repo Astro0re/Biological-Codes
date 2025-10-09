b0ab08ba568bc2e658b24b30c648147e7ba9f609
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-10-09 11:38:17 +0100

    code formulas

--- Phylogenetic tree.py
+++ Phylogenetic tree.py
@@ -1,7 +1,11 @@
 # Phylogenetic tree distance calculation function for DNA sequences.
 from DNA_Sequence import val , dna_gen , dna_ver , count_dna
+from Sequence_Alignment import s_a , diff_s_a , relational
 import pandas as pd
 
 def phylo_tree():
-    # formula = (1/3) * ln()
+    # n = number of differences between two sequences
+    # L = length of the sequences
+    # p = n/L
+    # formula = -(3/4) * ln(1-(4/3)*p)
     pass
