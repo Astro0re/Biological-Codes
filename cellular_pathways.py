e457c5cbb8f8d3f48e9268de0a54abab37e66773
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-12-04 12:50:25 +0100

    update

--- cellular_pathways.py
+++ cellular_pathways.py
@@ -1,1 +1,14 @@
 # Search cellular pathways, through system/function search or gene search
+
+
+# Conditional requirments that point to the type of cellular reaction taking place
+
+def temprature():
+    temp= int(input("At what temprature is this reaction occuring(C): "))
+    if temp > 45:
+        print('This reaction is exthermic')
+    elif temp < 30:
+        print('This reaction is endothermic')
+
+# Cell Fate Dicision Network
+# Outcomes: Apoptosis(death) or Prolifiration(replication)
