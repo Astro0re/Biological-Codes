00ef124a77b21e4959b4bf26b26bf87b91737aa3
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2026-06-13 19:34:11 +0100

    Details added

--- Protein_Folding.py
+++ Protein_Folding.py
@@ -1,1 +1,6 @@
 # Code to stimulate all possible folds a protein can achive given cretain parameters
+
+# Parameters: 
+# 1. Folding angles (max-min requirements)
+# 2. Bound type (Helix/Sheets)
+# 3. Chemical bining rules
