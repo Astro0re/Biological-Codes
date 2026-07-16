e79ce16621417c267f92e63fe2fdbd3ede0cedee
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2026-07-16 13:27:32 +0100

    Code to mimic cell growth and division

---
+++ Cellular/Cellular_division.py
@@ -0,0 +1,67 @@
+"""
+Code to stimulate celle division to a certain stage till a reset occurs
+
+cell starts at base value, cell division/growth occur, when threshold is reached cell divides to form a new organism and both continue to grow and divide (max division 10) determined by time input
+"""
+
+# Code takes in a time input in seconds to determine how big a cell will grow within that given time 
+
+
+def cell_div():
+    time = int(input("Input observation time(seconds): "))
+    expected_div = round(time / 20)
+    out_cell = {'cell0' : 0}
+    next_cell = 1
+
+    #for i in range(expected_div):
+    #    out_cell[f"cell{i}"] = 0
+    
+    for i in range(time): 
+        out_cell['cell0'] += 1
+        if out_cell['cell0'] >= 20: 
+            break
+    if (time - 20)  > 0: 
+        for i in range(expected_div):
+            out_cell[f"cell{i}"] = 0
+            for i in range(time - 20): 
+                out_cell[f'cell{i+1}'] += 1
+                if out_cell['cell{i+1}'] >= 20: 
+                    break
+
+      #      out_cell[f'cell{+1}']
+
+      #  else: 
+
+    
+    print(f"At the end of {time} seconds there are {len(out_cell)} cells")
+    print(out_cell)
+
+"""
+        out_cell[f"cell{1}"] = +1
+    if out_cell[f"cell{1}"] > 20:
+            out_cell[f"cell{1+1}"] = +1
+    print(f"At the end of {time} seconds there are {out_cell.keys()} cells")
+    print(f"{out_cell.keys()} has a size of {out_cell.values()}") 
+    
+
+
+def cell_div_1():
+    time = int(input("Input observation time(seconds): "))
+    expected_div = round(time / 20)
+    out_cell = {}
+    for i in range(expected_div):
+        out_cell[f"cell{i}"] = 0
+    for i in range(time): 
+        out_cell[f"cell{i}"] += 1 
+        if out_cell[f"cell{i}"] >= 20:
+            break
+    time_rema = time - 20 
+    for i in range(time_rema):
+        out_cell[f"cell{i+1}"] +=1
+        if out_cell[f"cell{0+1}"] >=20: 
+            break
+    print(f"At the end of {time} seconds there are {out_cell.keys()} cells")
+    print(f"{out_cell.keys()} has a size of {out_cell.values()}") 
+
+"""
+cell_div()
